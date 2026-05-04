5ca0c01ed0ea206e5d80587b95d38550498a4ad0
Rayene Zanina <rayene.zanina@mail.mcgill.ca>
2026-05-04 18:56:22 -0400

    fixed laplacian, added density update

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 import numpy as np
 
+time_steps = 300 # 3 hours
+
+for t in range(time_steps):
+    pass
--- model/population_growth.py
+++ model/population_growth.py
@@ -5,24 +5,27 @@ def drug_source(concentration, position, grid_dims):
     source[position] = concentration
     return source
 
-def update_concentration(concentration_grid, source, diffusion_rate, dt):
+def update_concentration(concentration_grid, source, diffusion_rate, dt = 0.01, dx = 0.1):
     laplacian = (np.roll(concentration_grid, 1, axis=0) +
                  np.roll(concentration_grid, -1, axis=0) +
                  np.roll(concentration_grid, 1, axis=1) +
                  np.roll(concentration_grid, -1, axis=1) -
-                 4 * concentration_grid)
+                 4 * concentration_grid) / dx**2
     return concentration_grid + (diffusion_rate * laplacian + source) * dt
 
-def update_concentration_3d(concentration_grid, source, diffusion_rate, dt):
+def update_concentration_3d(concentration_grid, source, diffusion_rate, dt = 0.01, dx = 0.1):
     laplacian = (np.roll(concentration_grid, 1, axis=0) +
                  np.roll(concentration_grid, -1, axis=0) +
                  np.roll(concentration_grid, 1, axis=1) +
                  np.roll(concentration_grid, -1, axis=1) +
                  np.roll(concentration_grid, 1, axis=2) +
                  np.roll(concentration_grid, -1, axis=2) -
-                 6 * concentration_grid)
+                 6 * concentration_grid) / dx**2
     return concentration_grid + (diffusion_rate * laplacian + source) * dt
 
 def drug_effect(concentration_grid, EC50 = 0.5, hill_coefficient = 1):
     return concentration_grid**hill_coefficient / (EC50**hill_coefficient + concentration_grid**hill_coefficient)
+
+def update_density(density_grid, drug_effect_grid, growth_rate = 0.05, alpha = 0.1, dt = 0.01):
+    return density_grid + (growth_rate * density_grid * (1 - density_grid) - alpha * drug_effect_grid * density_grid) * dt
 
